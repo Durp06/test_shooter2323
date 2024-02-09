d90f2c3a762756a5a282281a9cac280a03faa3ce
Durp06 <98856010+Durp06@users.noreply.github.com>
2024-02-09 12:10:52 -0500

    untested and unfinished addition of remote limit test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,12 +26,16 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
  * project.
  */
 public class Robot extends TimedRobot {
+  public boolean remote = false
 
+  if (remote == false){
   public int canIDs[] = new int[] {8, 9, 10, 11};
   public TalonFX motors[] = new TalonFX[canIDs.length];
 
-  //public TalonFX motor = new TalonFX(10);
-
+  }else{
+  public TalonFX motorLead = new TalonFX(1);
+  public TalonFX motorFollow = new TalonFX(2);
+  }
 
   private void configureDriveMotor(TalonFX y) {
     var driveConfig = new TalonFXConfiguration();
@@ -44,29 +48,71 @@ public class Robot extends TimedRobot {
     
     y.getConfigurator().apply(driveConfig);
     //motor2.getConfigurator().apply(driveConfig);
-}
+  }
+  private void configureLeadMotor(TalonFX motor){
+    var leadConfig = new TalonFXConfiguration();
+    leadConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    leadConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    leadConfig.Slot0.kP = 20.0;
+    leadConfig.Slot0.kI = 0;
+    leadConfig.Slot0.kD = 0;
+
+    //TODO add limit switch + remote limit switch
+
+    motor.getConfigurator().apply(leadConfig);
+  }
+
+  private void configureFollowMotor(TalonFX motor){
+    var followConfig = new TalonFXConfiguration();
+    followConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    followConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    followConfig.Slot0.kP = 20.0;
+    followConfig.Slot0.kI = 0;
+    followConfig.Slot0.kD = 0;
+
+    //TODO add limit switch + remote limit switch
+
+
+    motor.getConfigurator().apply(followConfig);
+  }
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
+    if (remote == false){
     for (int i = 0; i<canIDs.length; i++){
       motors[i] = new TalonFX(canIDs[i]);
        SmartDashboard.putNumber("PercentOut" + i, 0);
        configureDriveMotor(motors[i]);
     }
+    }else{
+
+    SmartDashboard.putNumber("PercentOutLeader", 0);
+    SmartDashboard.putNumber("PercentOutFollower", 0);
+
+    configureLeadMotor(motorLead);
+    configureFollowMotor(motorFollow);
+    }
   }
 
-  /*public double getPercentOut() {
+  public double getPercentOutLead() {
     return NetworkTableInstance
       .getDefault()
       .getTable("/SmartDashboard")
-      .getEntry("PercentOut")
+      .getEntry("PercentOutLeader")
       .getDouble(0.0);
-  }*/
+  }
 
- 
+ public double getPercentOutFollow() {
+    return NetworkTableInstance
+      .getDefault()
+      .getTable("/SmartDashboard")
+      .getEntry("PercentOutFollower")
+      .getDouble(0.0);
+  }
 
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
@@ -77,6 +123,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    
+    if (remote == false) {
     for (int x = 0; x < motors.length; x++){
       var request = new DutyCycleOut(
       NetworkTableInstance
@@ -89,13 +137,16 @@ public class Robot extends TimedRobot {
 
       SmartDashboard.putNumber("RPM " + x, motors[x].getRotorVelocity().getValue() * 60);
       SmartDashboard.putNumber("Voltage", motors[x].getTorqueCurrent().getValue());
+    }else{
+
+      var requestLead = new DutyCycle(getPercentOutLead);
+      var requestFollow = new DutyCycleOut(getPercentOutFollow);
+
+      motorLead.setControl(requestLead);
+      motorFollow.setControl(requestFollow);
+    }
     }
 
-    // var request1 = new DutyCycleOut(getPercentOut());
-    // motor.setControl(request1);
-
-    // SmartDashboard.putNumber("RPS", motor.getRotorVelocity().getValue());
-    // SmartDashboard.putNumber("RPM 1", motor.getRotorVelocity().getValue() * 60);
   }
  
 
